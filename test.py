5bff8a45ade05b52f189abeead50e00dd4003f9b
Chattercube <33176933+Chattercube@users.noreply.github.com>
2024-02-21 22:49:34 +0800

    uu

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+import asyncio
+
 for x in (False, True):
     for y in (False, True):
         print(x, y, x & y)
@@ -8,3 +10,18 @@ x = ['a', 'b', 'c']
 print(False ^ ('a' in x))
 
 print(timedelta(seconds=30) / timedelta(seconds=2))
+
+async def fun1():
+    print("Hello World")
+    await asyncio.sleep(1)
+    print("Not Hello World")
+
+async def fun2():
+    print("Hello World2")
+    await asyncio.sleep(1)
+    print("Not Hello World2")
+        
+
+asyncio.run(fun1())
+asyncio.run(fun2())
+
